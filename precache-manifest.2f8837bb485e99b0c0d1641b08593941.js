b52e0d127e4403292abc2324685bd895c662a6e2
rsongdalsong <jeongwon412@gmail.com>
2020-01-06 17:56:36 -0500

    Updates

---
+++ precache-manifest.2f8837bb485e99b0c0d1641b08593941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914bfd8e1dac3ab9b974747f5d50f566",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "83da4a041976334976f3",
+    "url": "/portfolio/static/css/main.3b0c4151.chunk.css"
+  },
+  {
+    "revision": "4d0412e50073dbb7ce5a",
+    "url": "/portfolio/static/js/2.ddb40a3f.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/portfolio/static/js/2.ddb40a3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83da4a041976334976f3",
+    "url": "/portfolio/static/js/main.68e77eeb.chunk.js"
+  },
+  {
+    "revision": "6fda17b92472121eb014",
+    "url": "/portfolio/static/js/runtime-main.ff0516c6.js"
+  }
+]);
