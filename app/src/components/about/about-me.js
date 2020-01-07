0d79cf8c524d3aa98f1359b42fd252c3df01991e
rsongdalsong <jeongwon412@gmail.com>
2020-01-06 19:08:26 -0500

    about me style fixed for mobile

--- app/src/components/about/about-me.js
+++ app/src/components/about/about-me.js
@@ -5,7 +5,6 @@ const AboutMeDivStyle = {
     margin:"1%",
     //border:"solid red",
     minWidth:"30%",
-    maxWidth:"60%",
 }
 
 const AboutMeContentStyle = {
@@ -17,16 +16,16 @@ const AboutMe = () =>{
         <div style={ AboutMeDivStyle } className="content">               
             <h2>About me</h2><hr/>
             <div style={ AboutMeContentStyle }>
-                <Row>
-                    <Col xs={ 3 } style={{padding:"1rem"}}>
-                        <Image src={ process.env.PUBLIC_URL + "/assets/me.jpg" } roundedCircle width="100%"></Image>                  
-                    </Col>
-                    <Col xs={ 9 } style={{textAlign:"left"}}>
+                <div style={{display:"flex", padding:"0", justifyContent: "center" }}>
+                    <div style={{margin: "0 2%"}}>
+                        <Image src={ process.env.PUBLIC_URL + "/assets/me.jpg" } roundedCircle width="150px"></Image>                  
+                    </div>
+                    <div style={ {textAlign:"left", margin: "0 2%"} }>
                         <p>Hello, my name is Jeongwon</p>
                         <p>A diligent, ambitious, and fast learning student in Software Engineering Science at Western University, who is willing to build experiences in
                             software designing, web-development, and machine learning.</p>
-                        </Col>
-                </Row>
+                    </div>
+                </div>
             </div>
         </div>
     );
